caafb657962bed7356c6ba554f5cbb65f00db77b
mortezadadgar <mortezadadgar97@gmail.com>
2021-04-28 17:04:41 +0430

    dwm: Increase bar height
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 0;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
+static const int user_bh            = 28;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "RobotoMono Nerd Font:size=10" };
 static const char dmenufont[]       = "RobotoMono Nerd Font:size=10";
