4a21432a83a24d635550b116032fd636cc38e292
mortezadadgar <mortezadadgar97@gmail.com>
2021-04-28 16:52:57 +0430

    dwm: Add clients rules
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- config.def.h
+++ config.def.h
@@ -32,9 +32,12 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/*  class               instance  title  tags  mask  isfloating  monitor  */  
+	{   "firefox",          NULL,     NULL,  1   << 1,   0,          -1  },
+	{   "TelegramDesktop",  NULL,     NULL,  1   << 2,   0,          -1  },
+	{   "mpv",              NULL,     NULL,  1   << 3,   0,          -1  },
+	{   "Zathura",          NULL,     NULL,  1   << 3,   0,          -1  },
+	{   "qBittorrent",      NULL,     NULL,  0,          1,          -1  },
 };
 
 /* layout(s) */
