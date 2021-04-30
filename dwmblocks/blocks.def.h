ae51008cb8e3876296f53a15a0e66092e8cb5345
mortezadadgar <mortezadadgar97@gmail.com>
2021-04-30 16:34:26 +0430

    Add dwmblocks
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

---
+++ dwmblocks/blocks.def.h
@@ -0,0 +1,13 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/                  /*Command*/           /*Interval*/    /*Signal*/
+	{""               ,       "statusbar -a",            0,            10,       },
+	{""               ,       "statusbar -t",            8,             0,       },
+	{""               ,       "statusbar -b",            8,             0,       },
+	{""               ,       "statusbar -w",            3,             0,       },
+	{"^c#61AFEF^ ^d^",       "date '+%b %d - %H:%M'",   60,             0,       },
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim[] = " | ";
+static unsigned int delimLen = 5;
