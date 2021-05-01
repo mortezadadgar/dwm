9319c7e42374bf6b6b390704746bcdd64f7e063b
mortezadadgar <mortezadadgar97@gmail.com>
2021-05-01 15:02:40 +0430

    dwmblocks: update modules
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,12 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/                  /*Command*/           /*Interval*/    /*Signal*/
+	{""               ,       "statusbar -m",            3,             0,       },
 	{""               ,       "statusbar -a",            0,            10,       },
 	{""               ,       "statusbar -t",            8,             0,       },
 	{""               ,       "statusbar -b",            8,             0,       },
 	{""               ,       "statusbar -w",            3,             0,       },
-	{"^c#61AFEF^󰃭 ^d^",       "date '+%b %d - %H:%M'",   60,             0,       },
+	{"^c#61AFEF^󰃭 ^d^",       "date '+%b %d - %H:%M'",   60,            0,       },
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
