6bfa37fb53fcfbd7d4181ab27b93f17574b98ef7
mortezadadgar <mortezadadgar97@gmail.com>
2021-07-12 16:24:21 +0430

    dwmblocks: Update command name
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,11 +1,11 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/                  /*Command*/           /*Interval*/    /*Signal*/
-	{""               ,       "dwm-block -m",            3,             0,       },
-	{""               ,       "dwm-block -a",            0,            10,       },
-	{""               ,       "dwm-block -t",            8,             0,       },
-	{""               ,       "dwm-block -b",            8,             0,       },
-	{""               ,       "dwm-block -w",            3,             0,       },
+	{""               ,       "dwm-status -m",            3,             0,       },
+	{""               ,       "dwm-status -a",            0,            10,       },
+	{""               ,       "dwm-status -t",            8,             0,       },
+	{""               ,       "dwm-status -b",            8,             0,       },
+	{""               ,       "dwm-status -w",            3,             0,       },
 	{"^c#61AFEF^󰃭 ^d^",       "date '+%b %d - %H:%M'",   60,            0,       },
 };
 
