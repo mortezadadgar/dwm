3a476faa904be4980cf60ca04c849662ab714d53
mortezadadgar <mortezadadgar97@gmail.com>
2021-09-03 11:16:19 +0430

    dwm: Applied actualfullscreen
    dwm-actualfullscreen-20191112-cb3f58a.diff
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- dwm.c
+++ dwm.c
@@ -234,6 +234,7 @@ static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
 static void togglescratch(const Arg *arg);
 static void togglesticky(const Arg *arg);
+static void togglefullscr(const Arg *arg);
 static void toggletag(const Arg *arg);
 static void toggleview(const Arg *arg);
 static void unfocus(Client *c, int setfocus);
@@ -1975,6 +1976,13 @@ togglesticky(const Arg *arg)
 		return;
 	selmon->sel->issticky = !selmon->sel->issticky;
 	arrange(selmon);
+}
+
+void
+togglefullscr(const Arg *arg)
+{
+  if(selmon->sel)
+    setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
