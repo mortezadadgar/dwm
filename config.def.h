2701b8ab0b0f494ebd0a30ba65b26ee23aa2fdcd
mortezadadgar <mortezadadgar97@gmail.com>
2021-10-09 23:19:54 +0330

    dwm: Update nnn command
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- config.def.h
+++ config.def.h
@@ -87,7 +87,7 @@ static const char *termcmd[]  = { TERM, NULL };
 /*First arg only serves to match against key in rules*/
 static const char *scratchpadcmd[] = {"s", TERM, "-t", "scratchpad", "-o", "window.dimensions.columns=80", "-o", "window.dimensions.lines=26" ,NULL};
 static const char *calc[] = {"c", TERM, "-t", "calc", "-e", "bc", "-q", NULL};
-static const char *nnn[] = {"n", TERM, "-t", "nnn", "-e", "nnn", "-eux", NULL};
+static const char *nnn[] = {"n", TERM, "-t", "nnn", "-e", "nnn", "-eu", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
