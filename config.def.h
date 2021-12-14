c1c974ae784823fac124a3d3aaf8eb88f6ec95da
mortezadadgar <mortezadadgar97@gmail.com>
2021-12-14 22:31:36 +0330

    Applied titlecolor
    dwm-titlecolor-20210815-ed3ab6b4.diff
    
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- config.def.h
+++ config.def.h
@@ -19,8 +19,9 @@ static char selbordercolor[]        = "#005577";
 static char selbgcolor[]            = "#005577";
 static char *colors[][3] = {
        /*               fg           bg           border   */
-       [SchemeNorm] = { normfgcolor, normbgcolor, normbordercolor },
-       [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
+       [SchemeNorm]   = { normfgcolor, normbgcolor, normbordercolor },
+       [SchemeSel]    = { selfgcolor,  selbgcolor,  selbordercolor  },
+       [SchemeTitle]  = { normfgcolor, normbgcolor, normbordercolor  },
 };
 
 /* tagging */
