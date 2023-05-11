6c972420cfe616ecfab4144225b3a1885c3b3b83
mortezadadgar <mortezadadgar97@gmail.com>
2023-05-11 16:24:36 +0330

    Update config
    Signed-off-by: mortezadadgar <mortezadadgar97@gmail.com>

--- config.def.h
+++ config.def.h
@@ -10,7 +10,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=9:style=Bold" };
+static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=9:style=Bold", "Vazirmatn UI NL:size=9:style=Bold" };
 static const char dmenufont[]       = "monospace:size=10";
 static char normbgcolor[]           = "#222222";
 static char normbordercolor[]       = "#444444";
