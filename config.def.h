7fb2abb2da281566e8f6d8e4e0dd02b1f6ca5717
mortezadadgar <mortezadadgar97@gmail.com>
2025-10-30 18:15:00 +0330

    Applied bar height

--- config.def.h
+++ config.def.h
@@ -15,6 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
+static const int user_bh            = 24;        /* 0 means that dwm will calculate bar height, >= 1 means dwm will user_bh as bar height */
 static const int focusonwheel       = 0;
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10:style=Bold", "Vazirmatn UI NL:size=10:style=Bold" };
 static const char dmenufont[]       = "monospace:size=10";
