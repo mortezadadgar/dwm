67fc13625efdc682f08570c003319bd615e98cd2
mortezadadgar <mortezadadgar97@gmail.com>
2025-11-08 20:14:39 +0330

    Applied cool_autostart

--- config.def.h
+++ config.def.h
@@ -31,6 +31,12 @@ static char *colors[][3] = {
        [SchemeSel]  = { selfgcolor,  selbgcolor,  selbordercolor  },
 };
 
+static const char *const autostart[] = {
+	"dwmbar", NULL,
+	"/home/morteza/.config/dwm/autostart.sh", NULL,
+	NULL /* terminate */
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
